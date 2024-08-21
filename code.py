aa4f93759feac624c44e208becde5880ef647c22
umtksa <umtksa@gmail.com>
2024-08-21 10:57:49 +0300

    Update code.py

--- code.py
+++ code.py
@@ -4,11 +4,7 @@ import re
 # Online txt dosyasını indir
 url = "https://raw.githubusercontent.com/umtksa/api/main/sentences.txt"
 response = requests.get(url)
-if response.status_code == 200:
-    sentences = response.text.splitlines()  # Her satırı bir eleman olarak alır
-else:
-    print("Dosya indirilemedi.")
-    sentences = []
+sentences = response.text.splitlines() if response.status_code == 200 else []
 
 # Regex patternleri
 patterns = {
@@ -26,52 +22,29 @@ patterns = {
 
 def identify_question_type(sentence):
     if '?' not in sentence:
-        return 'Belirlenemedi', sentence
-
+        return 'Belirlenemedi'
+    
     for category, pattern in patterns.items():
-        match = re.search(pattern, sentence, re.IGNORECASE)
-        if match:
-            return category, match.group()
+        if re.search(pattern, sentence, re.IGNORECASE):
+            return category
 
-    return 'Belirlenemedi', sentence
+    return 'Belirlenemedi'
 
 def process_sentences(sentences):
-    categorized_questions = {
-        'zaman': [],
-        'nasıl': [],
-        'naber': [],
-        'hangi': [],
-        'yer': [],
-        'fiyat': [],
-        'miktar': [],
-        'kişi': [],
-        'mı': [],
-        'ne': [],
-        'Belirlenemedi': []
-    }
+    categorized_questions = {category: [] for category in patterns.keys()}  # Kategorileri oluştur
+    categorized_questions['Belirlenemedi'] = []
 
     for sentence in sentences:
-        category, matched_part = identify_question_type(sentence)
-        if category == 'Belirlenemedi':
-            categorized_questions['Belirlenemedi'].append(sentence)
-        else:
-            categorized_questions[category].append(sentence)
+        category = identify_question_type(sentence)
+        categorized_questions[category].append(sentence)
 
     return categorized_questions
 
 # Sonuçları yazdırma
 categorized_questions = process_sentences(sentences)
 
-# Başlıkları bir kez yazdır ve altına o başlıktaki cümleleri yazdır
-for category in patterns.keys():
-    if categorized_questions[category]:
-        print(f"--- {category} soruları ---")
-        for question in categorized_questions[category]:
+for category, questions in categorized_questions.items():
+    if questions:
+        print(f"--- {category} ---")
+        for question in questions:
             print(f"- {question}")
-
-
-# Soru olmayan cümleleri en sona koy
-if categorized_questions['Belirlenemedi']:
-    print(f"--- soru olmayanlar ---")
-    for sentence in categorized_questions['Belirlenemedi']:
-        print(f"- {sentence}")
